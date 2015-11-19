b9e6cc2e5b1992263b693bf7c8cbab7257974341
Neil Martin <neil.martin@abilitation.com>
2015-11-19 14:52:13 +0000

    Updated Service Interfaces
    Updated service interfaces to correctly inherit from IService<>

--- src/DAL/UrfIdentity.DAL.Service/Interfaces/IApplicationRoleService.cs
+++ src/DAL/UrfIdentity.DAL.Service/Interfaces/IApplicationRoleService.cs
@@ -1,39 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading;
-using System.Threading.Tasks;
-using Repository.Pattern.Repositories;
+using Service.Pattern;
 using UrfIdentity.Models;
 
 
 namespace UrfIdentity.DAL.Service.Interfaces
 {
     public interface IApplicationRoleService
+        : IService<ApplicationRole>
     {
-        ApplicationRole Find(params object[] keyValues);
-        IQueryable<ApplicationRole> SelectQuery(string query, params object[] parameters);
-        void Insert(ApplicationRole entity);
-        void InsertRange(IEnumerable<ApplicationRole> entities);
-        void InsertOrUpdateGraph(ApplicationRole entity);
-        void InsertGraphRange(IEnumerable<ApplicationRole> entities);
-        void Update(ApplicationRole entity);
-        void Delete(object id);
-        void Delete(ApplicationRole entity);
-        IQueryFluent<ApplicationRole> Query();
-        IQueryFluent<ApplicationRole> Query(IQueryObject<ApplicationRole> queryObject);
-        IQueryFluent<ApplicationRole> Query(Expression<Func<ApplicationRole, bool>> query);
-        Task<ApplicationRole> FindAsync(params object[] keyValues);
-
-        Task<ApplicationRole> FindAsync(CancellationToken cancellationToken,
-            params object[] keyValues);
-
-        Task<bool> DeleteAsync(params object[] keyValues);
-
-        Task<bool> DeleteAsync(CancellationToken cancellationToken,
-            params object[] keyValues);
-
-        IQueryable<ApplicationRole> Queryable();
     }
 }
